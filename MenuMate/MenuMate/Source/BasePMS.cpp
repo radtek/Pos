3c06683c8391b379551a391817a0beb087fd512e
SHIVASHU <shivashu.sharma@menumate.com>
2017-04-03 16:34:18 +1200

    Branch_MM-13285 code pushed with BASEPMS

---
+++ MenuMate/MenuMate/Source/BasePMS.cpp
@@ -0,0 +1,72 @@
+//---------------------------------------------------------------------------
+
+
+#pragma hdrstop
+
+#include "BasePMS.h"
+
+//---------------------------------------------------------------------------
+
+#pragma package(smart_init)
+//TBasePMS *BasePMS;
+TBasePMS::TBasePMS() :fPhoenixNet(new TPhoenixNetTCPManager)
+{
+}
+//---------------------------------------------------------------------------
+//TBasePMS::TBasePMS()
+//{
+//}
+//---------------------------------------------------------------------------
+TBasePMS::~TBasePMS()
+{
+}
+//---------------------------------------------------------------------------
+void TBasePMS::Initialise()
+{
+}
+//---------------------------------------------------------------------------
+void TBasePMS::GetRoomStatus(TPhoenixRoomStatusExt &RoomStatus,AnsiString
+                                                       PMSIPAddress,int PMSPort)
+{
+}
+//---------------------------------------------------------------------------
+void TBasePMS::GetRoomStatus(std::vector<TSiHotAccounts> &siHotAccounts,
+                                           AnsiString pmsIPAddress,int pmsPort)
+{
+}
+//---------------------------------------------------------------------------
+bool TBasePMS::ExportData(TPaymentTransaction &PaymentTransaction, int StaffID)
+{
+}
+//---------------------------------------------------------------------------
+AnsiString TBasePMS::GetLastTransactionRef()
+{
+}
+//---------------------------------------------------------------------------
+bool TBasePMS::TestCode(AnsiString CodeToTest)
+{
+}
+//---------------------------------------------------------------------------
+void TBasePMS::ClearCodesTestedOk()
+{
+}
+//---------------------------------------------------------------------------
+bool TBasePMS::TestDefaultCodes()
+{
+}
+//---------------------------------------------------------------------------
+void TBasePMS::FindAndReplaceCode(AnsiString SourceCode, AnsiString DestCode)
+{
+}
+//---------------------------------------------------------------------------
+void TBasePMS::CheckCreditLimit(TPhoenixRoomCharge &RoomCharge,AnsiString
+                                                       PMSIPAddress,int PMSPort)
+{
+}
+//---------------------------------------------------------------------------
+void TBasePMS::ChargeRoom(TPhoenixRoomCharge &RoomCharge,AnsiString
+                                                       PMSIPAddress,int PMSPort)
+{
+}
+//---------------------------------------------------------------------------
+
---
+++ MenuMate/MenuMate/Source/BasePMS.h
@@ -0,0 +1,57 @@
+//---------------------------------------------------------------------------
+
+#ifndef BasePMSH
+#define BasePMSH
+#include <System.hpp>
+#include <memory>
+#include "MM_DBCore.h"
+#include "PHSTCPIP.h"
+#include "SiHotDataObjects.h"
+//---------------------------------------------------------------------------
+class TBasePMS
+{
+    public:
+        TBasePMS();
+        virtual ~TBasePMS();
+        void Intialise();
+        int TCPPort;
+        AnsiString TCPIPAddress;
+        int POSID;
+        bool Enabled;
+        AnsiString LastTransactionRef;
+        AnsiString GetRefNumber();
+
+        AnsiString DefaultPaymentCategory;
+        AnsiString DefaultItemCategory;
+        AnsiString CreditCategory;
+        AnsiString PointsCategory;
+        AnsiString RoundingCategory;
+        AnsiString DefaultTransactionAccount;
+        AnsiString DefaultSurchargeAccount;
+        AnsiString TipAccount;
+        AnsiString ExpensesAccount;
+
+
+        std::set<AnsiString> CodesTestedOk;
+        public :
+        bool Registered;
+        bool nabled;
+        TPhoenixHM(Database::TDBControl &inDBControl);
+        void virtual GetRoomStatus(TPhoenixRoomStatusExt &RoomStatus,AnsiString PMSIPAddress,int PMSPort);
+        bool virtual ExportData(TPaymentTransaction &PaymentTransaction, int StaffID);
+
+        AnsiString virtual GetLastTransactionRef();
+
+        void virtual Initialise();
+
+        std::auto_ptr<TPhoenixNetTCPManager>	fPhoenixNet;
+        bool virtual TestCode(AnsiString CodeToTest);
+        void virtual ClearCodesTestedOk();
+        bool virtual TestDefaultCodes();
+        void virtual FindAndReplaceCode(AnsiString SourceCode, AnsiString DestCode);
+        void virtual CheckCreditLimit(TPhoenixRoomCharge &RoomCharge,AnsiString PMSIPAddress,int PMSPort);
+        void virtual ChargeRoom(TPhoenixRoomCharge &RoomCharge,AnsiString PMSIPAddress,int PMSPort);
+        void virtual GetRoomStatus(std::vector<TSiHotAccounts> &siHotAccounts,AnsiString pmsIPAddress,int pmsPort);
+};
+//extern TBasePMS *BasePMS;
+#endif
