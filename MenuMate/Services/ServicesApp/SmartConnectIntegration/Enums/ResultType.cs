a3a46518623e8db780991def37f4f5e32f4ef1c0
Rupendra <rupendra.pandey@menumate.com>
2017-10-11 16:45:35 +1200

    libraries removed

--- MenuMate/Services/ServicesApp/SmartConnectIntegration/Enums/ResultType.cs
+++ MenuMate/Services/ServicesApp/SmartConnectIntegration/Enums/ResultType.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace SmartConnectIntegration.Enums
+﻿namespace SmartConnectIntegration.Enums
 {
     public enum ResultType
     {
