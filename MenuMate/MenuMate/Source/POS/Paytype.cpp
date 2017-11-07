7ad76e4ef6289d18ee634e8fa5fa1a9f5cd5e02b
Rupendra <rupendra.pandey@menumate.com>
2017-11-08 01:16:05 +1200

    first name and last name stored.

--- MenuMate/MenuMate/Source/POS/Paytype.cpp
+++ MenuMate/MenuMate/Source/POS/Paytype.cpp
@@ -1409,6 +1409,8 @@ void TfrmPaymentType::ProcessCreditPayment(TPayment *Payment)
                         else
                         {
                             CurrentTransaction.Customer.RoomNumber = atoi(frmPhoenixRoom->SelectedRoom.SiHotRoom.c_str());
+                            CurrentTransaction.Phoenix.FirstName =  frmPhoenixRoom->SiHotAccounts[frmPhoenixRoom->SelectedRoom.FolderNumber-1].AccountDetails[0].FirstName;
+                            CurrentTransaction.Phoenix.LastName =  frmPhoenixRoom->SiHotAccounts[frmPhoenixRoom->SelectedRoom.FolderNumber-1].AccountDetails[0].LastName;
                             TabName = frmPhoenixRoom->SelectedRoom.SiHotRoom;
                         }
                     }
@@ -1793,6 +1795,8 @@ void TfrmPaymentType::ProcessNormalPayment(TPayment *Payment)
                             else
                             {
                                 CurrentTransaction.Customer.RoomNumber = atoi(frmPhoenixRoom->SelectedRoom.SiHotRoom.c_str());
+                                CurrentTransaction.Phoenix.FirstName =  frmPhoenixRoom->SiHotAccounts[frmPhoenixRoom->SelectedRoom.FolderNumber-1].AccountDetails[0].FirstName;
+                                CurrentTransaction.Phoenix.LastName =  frmPhoenixRoom->SiHotAccounts[frmPhoenixRoom->SelectedRoom.FolderNumber-1].AccountDetails[0].LastName;
                                 TabName = frmPhoenixRoom->SelectedRoom.SiHotRoom;
                             }
                         }
