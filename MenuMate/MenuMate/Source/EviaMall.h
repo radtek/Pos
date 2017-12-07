5decfafd3bcc4658f18da78b373086cda9843914
Rupendra <rupendra.pandey@menumate.com>
2017-12-07 17:59:27 +1200

    new file added for mall.

---
+++ MenuMate/MenuMate/Source/EviaMall.h
@@ -0,0 +1,11 @@
+//---------------------------------------------------------------------------
+
+#ifndef EviaMallH
+#define EviaMallH
+#include "MallExport.h"
+//---------------------------------------------------------------------------
+class TEviaMall : public TMallExport
+{
+
+};
+#endif
