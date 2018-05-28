1d3545dd1e39a1b038de7fa08b10e0d2a003876d
Rupendra <rupendra.pandey@menumate.com>
2018-05-29 00:20:27 +1200

    Branch_MM-114245 logs reverted.

--- MenuMate/Services/ServicesApp/MenumateServices/WCFServices/WCFServicePaymentSense.cs
+++ MenuMate/Services/ServicesApp/MenumateServices/WCFServices/WCFServicePaymentSense.cs
@@ -17,29 +17,17 @@ namespace MenumateServices.WCFServices
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "WCFServicePaymentSense" in both code and config file together.
     public class WCFServicePaymentSense : IWCFServicePaymentSense
     {
-        private List<string> stringList;
         public WCFServicePaymentSense()
         {
-            stringList = new List<string>();
         }
 
         public PACTerminalWrapper GetAllCardTerminals(AuthorizationDetails autorizationDetails)
         {
-            stringList.Add("Request to GetAllCardTerminals");
-            WriteAndClearStringList();
             PACTerminalWrapper response = new PACTerminalWrapper();
-            stringList.Add("Constructor created.");
-            WriteAndClearStringList();
             try
             {
-                 stringList.Add("Before calling controller.");
-            WriteAndClearStringList();
                 PaymentSenseIntegrationController controller = new PaymentSenseIntegrationController();
-                stringList.Add("After creating instance of controller.");
-            WriteAndClearStringList();
                 response = controller.GetAllTerminals(autorizationDetails);
-                    stringList.Add("After calling GetAllTerminals function");
-            WriteAndClearStringList();
             }
             catch (Exception ex)
             {
@@ -70,7 +58,7 @@ namespace MenumateServices.WCFServices
             {
                 PaymentSenseIntegrationController controller = new PaymentSenseIntegrationController();
                 response = controller.DoTransaction(autorizationDetails, request);
-                
+
             }
             catch (Exception ex)
             {
@@ -93,65 +81,6 @@ namespace MenumateServices.WCFServices
                 ServiceLogger.Log("Exception in DoPurchase " + ex.Message);
             }
             return reportPrinted;
-        }
-
-        private void WriteToFile(List<string> list)
-        {
-            try
-            {
-                list.Add("=================================================================================");
-                string path = System.IO.Path.GetDirectoryName(
-                          System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase);
-
-
-                string location = Path.Combine(path, "Payment Sense Logs");
-                if (location.Contains(@"file:\"))
-                {
-                    location = location.Replace(@"file:\", "");
-                }
-                if (!Directory.Exists(location))
-                    Directory.CreateDirectory(location);
-
-                string name2 = "PaymentSenseLogs " + DateTime.Now.ToString("ddMMMyyyy") + ".txt";
-                string fileName = Path.Combine(location, name2);
-
-                if (fileName.Contains(@"file:\"))
-                {
-                    fileName = fileName.Replace(@"file:\", "");
-                }
-                if (!File.Exists(fileName))
-                {
-
-                    using (StreamWriter sw = File.CreateText(fileName))
-                    {
-                        for (int i = 0; i < list.Count; i++)
-                        {
-                            sw.WriteLine(list[i]);
-                        }
-                    }
-                }
-                else
-                {
-                    using (var sw = File.AppendText(fileName))
-                    {
-                        for (int i = 0; i < list.Count; i++)
-                        {
-                            sw.WriteLine(list[i]);
-                        }
-                    }
-                }
-                list.Add("  ");
-            }
-            catch (Exception ex)
-            {
-                ServiceLogger.Log("Exception in Making File" + ex.Message);
-            }
-        }
-
-        private void WriteAndClearStringList()
-        {
-            WriteToFile(stringList);
-            stringList.Clear();
         }
     }
 }
