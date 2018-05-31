aa5ea29645845b7a7cd1aa7047f24303fbd7e7e8
Rupendra <rupendra.pandey@menumate.com>
2018-05-31 19:06:05 +1200

    Branch_SmartConnect_Live url pointed to live.

--- MenuMate/Services/ServicesApp/SmartConnectIntegration/SmartConnectConstraints.cs
+++ MenuMate/Services/ServicesApp/SmartConnectIntegration/SmartConnectConstraints.cs
@@ -2,8 +2,8 @@
 {
     public class SmartConnectConstraints
     {
-        public const string PairingBaseAddress = "https://api-dev.smart-connect.cloud/POS/Pairing";
-        public const string TransactionBaseAddress = "https://api-dev.smart-connect.cloud/POS/Transaction";
+        public const string PairingBaseAddress = "https://api.smart-connect.cloud/POS/Pairing";
+        public const string TransactionBaseAddress = "https://api.smart-connect.cloud/POS/Transaction";
         //public const string PairingBaseAddress = "https://api-dev.smart-connect.cloud/POS/Pairing";
         //public const string TransactionBaseAddress = "https://api.smart-connect.cloud/POS/Transaction";
         public const string ContentType = "application/x-www-form-urlencoded";
