97cb1117d67c347fe46ee6fcc886cdf23359eef2
Rupendra <rupendra.pandey@menumate.com>
2018-08-03 02:27:08 +1200

    Branch_MM-120969 chefmate ui

--- ChefMate/Chefmate.UI/Views/SummaryView.xaml.cs
+++ ChefMate/Chefmate.UI/Views/SummaryView.xaml.cs
@@ -24,19 +24,12 @@ namespace Chefmate.UI.Views
 
     public class PercentageConverter : IValueConverter
     {
-        public object Convert(object value,
-            Type targetType,
-            object parameter,
-            System.Globalization.CultureInfo culture)
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            return System.Convert.ToDouble(value) *
-                   System.Convert.ToDouble(parameter);
+            return System.Convert.ToDouble(value) * System.Convert.ToDouble(parameter);
         }
 
-        public object ConvertBack(object value,
-            Type targetType,
-            object parameter,
-            System.Globalization.CultureInfo culture)
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
             throw new NotImplementedException();
         }
