3cddd031daced656c7b0f10e1ecb7b1bfd8eda33
Shivashu <shivashu.sharma@menumate.com>
2018-11-01 23:58:46 +1200

    Release 6.55.0.0 pushed contains cases 125643, 126020, 120920, 126019

--- MenuMate/Services/ServicesApp/MewsIntegration/Utilities/MewsRequestAddress.cs
+++ MenuMate/Services/ServicesApp/MewsIntegration/Utilities/MewsRequestAddress.cs
@@ -10,7 +10,7 @@
         public const string SpaceIds = "/api/connector/v1/spaces/getAll/";
         public const string AddOrder = "/api/connector/v1/orders/add";
         public const string AddBill = "/api/connector/v1/outletBills/add";
-        public const string ClientToken = "E0D439EE522F44368DC78E1BFB03710C-D24FB11DBE31D4621C4817E028D9E1D";
-        //public const string ClientToken = "DE25AC50EC564CDE8DF59B914B8AC03F-2ED92DEA9B15B14C31567BE6AC22F46";
+        //public const string ClientToken = "E0D439EE522F44368DC78E1BFB03710C-D24FB11DBE31D4621C4817E028D9E1D";//Stagging
+        public const string ClientToken = "DE25AC50EC564CDE8DF59B914B8AC03F-2ED92DEA9B15B14C31567BE6AC22F46";//Live
     }
 }
