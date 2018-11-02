f7ef0fc47a0b71461e99d1f507967cd17720989a
Shivashu <shivashu.sharma@menumate.com>
2018-11-02 16:55:32 +1200

    Branch_MM-WeChat code pushed.

--- MenuMate/Services/ServicesApp/WalletPaymets/Controller/Impl/WeChatWalletController.cs
+++ MenuMate/Services/ServicesApp/WalletPaymets/Controller/Impl/WeChatWalletController.cs
@@ -505,7 +505,8 @@ namespace WalletPayments.Controller.Impl
                 string path = System.IO.Path.GetDirectoryName(
                           System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase);
 
-                string location = Path.Combine(path, "logs");
+                string location = path;
+                //Path.Combine(path, "logs");
                 if (location.Contains(@"file:\"))
                 {
                     location = location.Replace(@"file:\", "");
