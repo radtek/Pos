16672c2c343f70bc5f1ff4ca460b4b4900a9401e
Rupendra Pandey <rupendra.pandey@menumate.com>
2018-11-26 18:40:18 +1200

    Branch_MM-129240 API CHANGES TO V3

--- MenuMate/Services/ServicesApp/Loyaltymate/Sevices/RequestAddress.cs
+++ MenuMate/Services/ServicesApp/Loyaltymate/Sevices/RequestAddress.cs
@@ -2,34 +2,33 @@
 {
     public class RequestAddress
     {
-        public const string BaseAddress = "http://menumateloyalty.com/";
-       // public const string BaseAddress = "http://menumatepos.com/";
+       // public const string BaseAddress = "http://menumateloyalty.com/";
+        public const string BaseAddress = "http://menumatepos.com/";
 //"http://menumateloyalty.com/";"http://lmonlineordering.azurewebsites.net/"
-     
 
         //Member
-        public const string SaveMember = "api/MemberApi/Save";
-        public const string GetAllMember = "api/MemberApi/MembersByCompany";
-        public const string GetMemberByUniqueId = "api/MemberApi/MemberByUniqueId";
-        public const string GetMemberByCardCode = "api/MemberApi/MemberByCode";
-        public const string GetMemberByEmail = "api/MemberApi/GetMemberByEmail";
-        public const string UpdateMemberCardCode = "api/v2Pos/MemberApi/UpdateMemberCardCode";
+        public const string SaveMember = "api/v3Pos/MemberApi/Save";
+        public const string GetAllMember = "api/v3Pos/MemberApi/MembersByCompany";
+        public const string GetMemberByUniqueId = "api/v3Pos/MemberApi/MemberByUniqueId";
+        public const string GetMemberByCardCode = "api/v3Pos/MemberApi/MemberByCode";
+        public const string GetMemberByEmail = "api/v3Pos/MemberApi/GetMemberByEmail";
+        public const string UpdateMemberCardCode = "api/v3Pos/MemberApi/UpdateMemberCardCode";
         //Points Transaction
-        public const string PostTransaction = "api/MembershipTransaction/Save";
-        public const string PostInvoiceTransaction = "api/MembershipTransaction/InvoiceTransaction";
+        public const string PostTransaction = "api/v3Pos/MembershipTransaction/Save";
+        public const string PostInvoiceTransaction = "api/v3Pos/MembershipTransaction/InvoiceTransaction";
         //Company
-        public const string GetCompanyDetail = "api/CompanyApi/Company";
+        public const string GetCompanyDetail = "api/v3Pos/CompanyApi/Company";
 
         //Gift Voucher
-        public const string GetGiftCardBalance = "api/VoucherApi/GiftCard";
-        public const string GetGiftCardInfo = "api/v2Pos/VoucherApi/GiftCardInfo";
+        public const string GetGiftCardBalance = "api/v3Pos/VoucherApi/GiftCard";
+        public const string GetGiftCardInfo = "api/v3Pos/VoucherApi/GiftCardInfo";
         //Pocket Voucher
-        public const string GetPocketVoucher = "api/VoucherApi/PocketVoucher";
+        public const string GetPocketVoucher = "api/v3Pos/VoucherApi/PocketVoucher";
 
         //Voucher Processing
-        public const string ProcessVoucherTransaction = "api/v2Pos/VoucherApi/ProcessVouchers";
+        public const string ProcessVoucherTransaction = "api/v3Pos/VoucherApi/ProcessVouchers";
         
-        public const string ReleaseVouchers = "api/VoucherApi/ReleaseVouchers";
+        public const string ReleaseVouchers = "api/v3Pos/VoucherApi/ReleaseVouchers";
 
         public const string SyncSiteMenu = "api/v5/MenuConsumableApi/SyncSiteMenu";
 
