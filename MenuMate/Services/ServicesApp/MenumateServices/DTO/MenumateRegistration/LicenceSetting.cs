d48c24ae133e2bc6c4a5cd42d148dcaa15d40191
Rupendra Pandey <rupendra.pandey@menumate.com>
2019-01-17 22:49:40 +1200

    Branch_MM-131458 data models created..

---
+++ MenuMate/Services/ServicesApp/MenumateServices/DTO/MenumateRegistration/LicenceSetting.cs
@@ -0,0 +1,16 @@
+﻿using MenumateServices.DTO.MenumateRegistration.Enums;
+
+namespace MenumateServices.DTO.MenumateRegistration   
+{
+    public class LicenceSetting
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsEnabledByDefault { get; set; }
+        public LicenceType SettingType { get; set; }
+        public int SettingSubType { get; set; }
+       // public SettingDisplayType SettingDisplayType { get; set; }
+       // public SettingValueType SettingValueType { get; set; }
+    }
+}
+    
---
+++ MenuMate/Services/ServicesApp/MenumateServices/DTO/MenumateRegistration/Site.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+
+namespace MenumateServices.DTO.MenumateRegistration
+{
+    public class Site
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public long SiteCode { get; set; }
+        public long CompanyId { get; set; }
+      //  public Company Company { get; set; }
+        public List<Terminal> Terminals { get; set; }
+        public long? SiteProfileId { get; set; }
+    //    public Profile SiteProfile { get; set; }
+       // public List<SiteTransactionSettingMapping> SiteTransactionSettingMappings { get; set; }
+
+    }
+}
