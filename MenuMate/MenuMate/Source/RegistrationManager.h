8ca18a25f4a38851ef4aa21af6473cf0e650bd47
Rupendra Pandey <rupendra.pandey@menumate.com>
2019-01-22 18:29:42 +1200

    Branch_MM-131458

---
+++ MenuMate/MenuMate/Source/RegistrationManager.h
@@ -0,0 +1,10 @@
+//---------------------------------------------------------------------------
+
+#ifndef RegistrationManagerH
+#define RegistrationManagerH
+//---------------------------------------------------------------------------
+class TRegistrationManager
+{
+
+};
+#endif
