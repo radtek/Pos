c8af627a93f47fa41df1228924278fc015380721
Rupendra Pandey <rupendra.pandey@menumate.com>
2019-01-23 21:49:10 +1200

    Branch_MM-131458

---
+++ MenuMate/Services/ServicesApp/RegistrationIntegration/Models/ApiRegistrationResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RegistrationIntegration.ViewModels
+{
+    public class ApiRegistrationResponse
+    {
+        public bool IsSuccessful { get; set; }
+        public string Message { get; set; }
+    }
+}
