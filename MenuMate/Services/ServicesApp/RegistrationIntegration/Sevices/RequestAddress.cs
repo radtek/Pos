5280efca3760b621634567fea64a43c579f53ae3
Lalit <lalit.singh@menumate.com>
2019-02-15 02:10:44 +1200

    Release 6.60.0.2 merged case 2205, 2257, 2240

--- MenuMate/Services/ServicesApp/RegistrationIntegration/Sevices/RequestAddress.cs
+++ MenuMate/Services/ServicesApp/RegistrationIntegration/Sevices/RequestAddress.cs
@@ -2,8 +2,8 @@
 {
     public class RequestAddress
     {
-        //public const string BaseAddress = "http://mmregistrations.azurewebsites.net/";
-       public const string BaseAddress = "http://mmregistrations-stg-mmregistrations.azurewebsites.net/";//"http://192.168.0.120/Menumatem/";//;
+        public const string BaseAddress = "http://mmregistrations.azurewebsites.net/";
+       //public const string BaseAddress = "http://mmregistrations-stg-mmregistrations.azurewebsites.net/";//"http://192.168.0.120/Menumatem/";//;
        public const string PosTerminalDetails = "api/v1/TerminalApi/PostTerminalDetail";
        public const string ValidateCompanyInfo = "api/v1/CompanyApi/GetValidateCompanyInfo";
 
